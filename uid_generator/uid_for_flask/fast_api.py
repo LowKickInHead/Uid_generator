5f7ac529feed4ce5907092d60089266542c12874
Господин Ведущий <sustaff@mail.ru>
2022-07-26 11:16:48 +0300

    Добавлен запуск uvicorn

--- uid_generator/uid_for_flask/fast_api.py
+++ uid_generator/uid_for_flask/fast_api.py
@@ -1,11 +1,12 @@
 import my_module
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
-@app.get("/api")
+@app.get("/")
 async def root():
     new = my_module.uid_generate()
     return new
 
-print("тест записи")
+
+uvicorn.run(app)
